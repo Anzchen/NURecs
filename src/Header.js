c2076ddbcab2a95d40bb9443508f024fed45ab09
Anzchen <Anthonyzchen@yahoo.com>
2023-02-12 13:00:07 -0500

    added header

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+export function Header() {
+  return (
+    <header className="header">
+      <h1>Oasis Taquería</h1>
+
+      <p className="sub-headline">
+        <b>We've swapped out our keyboards 💻 for tortilla presses 🌮</b>
+      </p>
+
+      <p>
+        With salsas are as spicy as our code, we offer members a delicious break
+        from the screen that will tantalize taste buds. We'll have a new taco
+        ready for you every time you come back. Make sure to visit us often!
+      </p>
+    </header>
+  );
+}
