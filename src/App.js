fe3a79780ede2034b8615813a76b633567326f89
Anzchen <Anthonyzchen@yahoo.com>
2023-02-26 13:58:29 -0500

    Client side routing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import { Header } from './Components/Header';
 import { Footer } from './Components/Footer';
-import './App.css';
-
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Restaurant from "./Restaurant";
+import { useEffect} from "react";
+import "./App.css";
 function App() {
-  return (
-<div className="page-wrapper">
-      <Header />
-          <>
-            <div className="highlights-menu">
-              <div className="highlights-menu-row row-span-3">
-                {/* <TacoComponent name="Base">
-                  {recipe.bases.map((b) => (
-                    <MenuItemLink
-                      key={b.url}
-                      url={b.url}
-                      name={b.name}
-                      description={truncate(b.recipe_text, 150)}
-                      tags={b.tags}
-                    />
-                  ))}
-                </TacoComponent> */}
-              </div>
-            </div>
-          </>
 
-      <div style={{ marginTop: `7.5rem` }}>
-        <Footer />
-      </div>
+  useEffect(() => {
+    document.title = "NURecs";  
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 style={{float: 'start'}}>NURecs</h1>
+      <Header />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Restaurant">Restaurant</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/restaurant" element={<Restaurant />}></Route>
+      </Routes>
+      <Footer />
     </div>
   );
 }
-
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>This is the Home page of the application</p>
+    </div>
+  );
+};
+export default Home;
