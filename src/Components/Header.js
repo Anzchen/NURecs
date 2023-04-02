d90d48e2580cad9a0dcdc9e545a2d47916ce8d2e
Adrian Tam <tam.ad@northeastern.edu>
2023-04-02 14:32:14 -0400

    Home Page Image

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,17 @@
 import "./Header.css";
 
+
 export function Header() {
   return (
-    <header className="header">
-      <p className="sub-headline">
-        <b>Find your favorites!📍</b>
-      </p>
-      <p>
-        With so many options, its hard to find what you love at Northeastern. Find new places to visit!
-      </p>
-    </header>
+    <div>
+      <header className="header">
+        <p className="sub-headline">
+          <b></b>
+        </p>
+        <p>
+          With so many options, its hard to find what you love at Northeastern. Find new places to visit!
+        </p>
+      </header>
+    </div>
   );
 }
